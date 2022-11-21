520587005a67bc8bcbc0cdad654065e9aafbb3a8
Ariq Pradipa Santoso <70847073+ariqpradipa@users.noreply.github.com>
2022-11-21 08:24:39 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Defaults
-OSRM_DATA_PATH=${OSRM_DATA_PATH:="/data"}
-OSRM_DATA_LABEL=${OSRM_DATA_LABEL:="java"}
-OSRM_GRAPH_PROFILE=${OSRM_GRAPH_PROFILE:="car"}
-OSRM_GRAPH_PROFILE_URL=${OSRM_GRAPH_PROFILE_URL:=""}
-OSRM_PBF_URL=${OSRM_PBF_URL:="http://download.geofabrik.de/asia/indonesia/java-latest.osm.pbf"}
+# # Defaults
+# OSRM_DATA_PATH=${OSRM_DATA_PATH:="/data"}
+# OSRM_DATA_LABEL=${OSRM_DATA_LABEL:="java-latest"}
+# OSRM_GRAPH_PROFILE=${OSRM_GRAPH_PROFILE:="car"}
+# OSRM_GRAPH_PROFILE_URL=${OSRM_GRAPH_PROFILE_URL:=""}
+# OSRM_PBF_URL=${OSRM_PBF_URL:="http://download.geofabrik.de/asia/indonesia/java-latest.osm.pbf"}
 OSRM_MAX_TABLE_SIZE=${OSRM_MAX_TABLE_SIZE:="8000"}
 
 
@@ -15,25 +15,25 @@ _sig() {
 trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
 
-# Retrieve the PBF file
-curl -L $OSRM_PBF_URL --create-dirs -o $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
+# # Retrieve the PBF file
+# curl -L $OSRM_PBF_URL --create-dirs -o $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
 
-# Set the graph profile path
-OSRM_GRAPH_PROFILE_PATH="/opt/$OSRM_GRAPH_PROFILE.lua"
+# # Set the graph profile path
+# OSRM_GRAPH_PROFILE_PATH="/opt/$OSRM_GRAPH_PROFILE.lua"
 
-# If the URL to a custom profile is provided override the default profile
-if [ ! -z "$OSRM_GRAPH_PROFILE_URL" ]; then
-    # Set the custom graph profile path
-    OSRM_GRAPH_PROFILE_PATH="/opt/custom-profile.lua"
-    # Retrieve the custom graph profile
-    curl -L $OSRM_GRAPH_PROFILE_URL --create-dirs -o $OSRM_GRAPH_PROFILE_PATH
-fi
+# # If the URL to a custom profile is provided override the default profile
+# if [ ! -z "$OSRM_GRAPH_PROFILE_URL" ]; then
+#     # Set the custom graph profile path
+#     OSRM_GRAPH_PROFILE_PATH="/opt/custom-profile.lua"
+#     # Retrieve the custom graph profile
+#     curl -L $OSRM_GRAPH_PROFILE_URL --create-dirs -o $OSRM_GRAPH_PROFILE_PATH
+# fi
 
-# Build the graph
-osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf -p $OSRM_GRAPH_PROFILE_PATH
-osrm-contract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm
+# # Build the graph
+# osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf -p $OSRM_GRAPH_PROFILE_PATH
+# osrm-contract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm
 
 # Start serving requests
-osrm-routed $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm --max-table-size $OSRM_MAX_TABLE_SIZE &
+osrm-routed /data/java-latest.osrm --max-table-size $OSRM_MAX_TABLE_SIZE &
 child=$!
 wait "$child"
