281a4055774885c2a41e6c794d361d54c2df32b0
Ariq Pradipa Santoso <70847073+ariqpradipa@users.noreply.github.com>
2022-11-21 07:56:44 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
-DATA_PATH=${DATA_PATH:="/osrm-data"}
+
+# Defaults
+OSRM_DATA_PATH=${OSRM_DATA_PATH:="/data"}
+OSRM_DATA_LABEL=${OSRM_DATA_LABEL:="java"}
+OSRM_GRAPH_PROFILE=${OSRM_GRAPH_PROFILE:="car"}
+OSRM_GRAPH_PROFILE_URL=${OSRM_GRAPH_PROFILE_URL:=""}
+OSRM_PBF_URL=${OSRM_PBF_URL:="http://download.geofabrik.de/asia/indonesia/java-latest.osm.pbf"}
+OSRM_MAX_TABLE_SIZE=${OSRM_MAX_TABLE_SIZE:="8000"}
+
 
 _sig() {
   kill -TERM $child 2>/dev/null
 }
+trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
-if [ "$1" = 'osrm' ]; then
-  trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
-  if [ ! -f $DATA_PATH/$2.osrm ]; then
-    if [ ! -f $DATA_PATH/$2.osm.pbf ]; then
-      curl $3 > $DATA_PATH/$2.osm.pbf
-    fi
-    ./osrm-extract $DATA_PATH/$2.osm.pbf
-    ./osrm-contract $DATA_PATH/$2.osrm
-  fi
+# Retrieve the PBF file
+curl -L $OSRM_PBF_URL --create-dirs -o $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
 
-  ./osrm-routed $DATA_PATH/$2.osrm --max-table-size 8000 &
-  child=$!
-  wait "$child"
-else
-  exec "$@"
+# Set the graph profile path
+OSRM_GRAPH_PROFILE_PATH="/osrm-profiles/$OSRM_GRAPH_PROFILE.lua"
+
+# If the URL to a custom profile is provided override the default profile
+if [ ! -z "$OSRM_GRAPH_PROFILE_URL" ]; then
+    # Set the custom graph profile path
+    OSRM_GRAPH_PROFILE_PATH="/osrm-profiles/custom-profile.lua"
+    # Retrieve the custom graph profile
+    curl -L $OSRM_GRAPH_PROFILE_URL --create-dirs -o $OSRM_GRAPH_PROFILE_PATH
 fi
+
+# Build the graph
+osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf -p $OSRM_GRAPH_PROFILE_PATH
+osrm-contract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm
+
+# Start serving requests
+osrm-routed $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm --max-table-size $OSRM_MAX_TABLE_SIZE &
+child=$!
+wait "$child"
